df505aacd78d23c8e967d806893e195f1cfb3de8
T0rRA <48518584+T0rRA@users.noreply.github.com>
2021-05-02 16:52:13 +0200

    Implementert metoder for å skrive til csv og for å skrive til fil. Skriv() med csv strat og skriv() med binær strat

--- src/main/java/org/openjfx/filSkriver.java
+++ src/main/java/org/openjfx/filSkriver.java
@@ -3,17 +3,29 @@ package org.openjfx;
 //Les tilfil
 //Strategy Patter'n brukt
 
+import java.io.*;
+
 interface FilLeseOppforsel {
-    public void skriv();
+    public void skriv(File file, Produkt produkt, ProduktKategori produktKategori) throws IOException;
 }
+
 // Må endre
 //Produkter er lagret som csv
 class CSVStrategy implements FilLeseOppforsel {
-    public void skriv() { // (Produkt produkt, ProduktKategori produktkategori, File file)
-        //implementer her: metode for å skrive til csv
-        final String kommaSeparerer=",";
-
-
+    //medode som skriver til CSV når CSVStrategy er valgt.
+    public void skriv(File file, Produkt produkt, ProduktKategori produktKategori) throws IOException {
+        //filenotfound, ioexeption
+        /*eventuelt:
+        ArrayList<Produkt> produktListe
+        for (Produkt : produktListe){}*/
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
+        StringBuffer enLinje = new StringBuffer();
+        enLinje.append(produkt.getProduktNavn());
+        enLinje.append(";");
+        enLinje.append(produkt.getOmProdukt());
+        bufferedWriter.write(enLinje.toString());
+        bufferedWriter.flush();
+        bufferedWriter.close();
 
         System.out.println("Skriver til CSV");
 
@@ -22,12 +34,19 @@ class CSVStrategy implements FilLeseOppforsel {
 
 //Produktkategori er lagret binært
 class BinaryStrategy implements FilLeseOppforsel {
-    public void skriv() {
-        //implementer her metode for å skrive binærfil
-
-        System.out.println("Skriver til Binær");
+    @Override
+    public void skriv(File file, Produkt produkt, ProduktKategori produktKategori) throws IOException {
+        try (FileOutputStream fileUt = new FileOutputStream(file);
+             ObjectOutputStream objektUt = new ObjectOutputStream(fileUt)) {
+            objektUt.writeObject(file);
+            objektUt.close();
+            System.out.println("Skriver til Binær");
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 }
+
 abstract class strategiVelger {
     private FilLeseOppforsel filLeseOppforsel;
 
@@ -35,8 +54,8 @@ abstract class strategiVelger {
         this.filLeseOppforsel = filLeseOppforsel;
     }
 
-    public void skrivTilFil() {
-        filLeseOppforsel.skriv();
+    public void skrivTilFil(File file, Produkt produkt, ProduktKategori produktKategori) throws IOException {
+        filLeseOppforsel.skriv(file, produkt, produktKategori);
     }
 
     public void setFilLeseOppforsel(FilLeseOppforsel filType) {
