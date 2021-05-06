241d45417825d7d7e2abae329b83191e598470a9
T0rRA <48518584+T0rRA@users.noreply.github.com>
2021-05-07 00:52:24 +0200

    Fikset csv klasse så leggTil og fjern kan kalles og de skriver til fil

--- src/main/java/org/openjfx/Produkt.java
+++ src/main/java/org/openjfx/Produkt.java
@@ -1,17 +1,22 @@
 package org.openjfx;
 import com.opencsv.bean.CsvBindByPosition;
 
-public class Produkt {
+import java.io.Serializable;
+
+public class Produkt implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     public Produkt(String produktNavn, String omProdukt, String produktKategori) {
         this.produktNavn = produktNavn;
         this.omProdukt = omProdukt;
         this.produktKategori = produktKategori;
     }
-    @CsvBindByPosition(position = 0)
+
     private String produktNavn;
-    @CsvBindByPosition(position = 1)
+
     private String omProdukt;
-    @CsvBindByPosition(position = 2)
+
     private String produktKategori;
 
     public String getProduktKategori() { return produktKategori; }
