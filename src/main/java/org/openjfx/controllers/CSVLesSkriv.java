863cf09f28c9c5ad5d6a632c2244225375a4757e
T0rRA <48518584+T0rRA@users.noreply.github.com>
2021-05-06 18:25:44 +0200

    *skriver til fil rett fra objekt :D

--- src/main/java/org/openjfx/controllers/CSVLesSkriv.java
+++ src/main/java/org/openjfx/controllers/CSVLesSkriv.java
@@ -1,16 +1,26 @@
 package org.openjfx.controllers;
+import com.opencsv.CSVWriter;
 import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import org.openjfx.Produkt;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class CSVLesSkriv {
-    //returnerer liste av produkter
-    public  List<Produkt> lesCSV(){
-    String filNavn = "CSV.csv";
+    //Leser CSV, konverterer til Liste<produkt>
+    public List<Produkt> lesCSV() {
+        String filNavn = "CSV.csv";
 
         List<Produkt> produktListe = null;
         try {
@@ -21,5 +31,25 @@ public class CSVLesSkriv {
 
         produktListe.forEach(System.out::println); //fjern senere
         return produktListe;
+    }
+
+    public void skrivTilCSV(List<Produkt> produkts) {
+        try (
+
+                Writer writer = Files.newBufferedWriter(Paths.get("csv.csv"));
+        ) {
+            StatefulBeanToCsv<Produkt> beanToCsv = new StatefulBeanToCsvBuilder(writer)
+                    .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER)
+                    .build();
+
+            beanToCsv.write(produkts);
+
+        } catch (CsvRequiredFieldEmptyException e) {
+            e.printStackTrace();
+        } catch (CsvDataTypeMismatchException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
